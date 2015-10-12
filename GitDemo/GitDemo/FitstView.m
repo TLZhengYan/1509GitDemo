476c86d2f7df375b61c4c54c1b66d6e67676fcd6
TLZhengYan <778786990@qq.com>
2015-10-12 11:49:17 +0800

    qqqqqqq

---
+++ GitDemo/GitDemo/FitstView.m
@@ -0,0 +1,21 @@
+//
+//  FitstView.m
+//  GitDemo
+//
+//  Created by Qianfeng on 15/10/12.
+//  Copyright (c) 2015年 图灵. All rights reserved.
+//
+
+#import "FitstView.h"
+
+@implementation FitstView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
